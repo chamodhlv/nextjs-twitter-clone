7e067256a84282e767c5fc8e95286a07e7146d58
chamodhlv <chamodh300@gmail.com>
2026-08-06 02:00:27 +0530

    Add Navbar and MobileNavbar components; refactor layout and update dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,11 @@
 import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ui/ModeToggle";
+import ModeToggle from "@/components/ModeToggle";
 
 export default function Home() {
   return (
     <div>
-      <Show when="signed-out">
-        <SignInButton>
-          <Button>Sign In</Button>
-        </SignInButton>
-        <SignUpButton>
-          <Button>Sign Up</Button>
-        </SignUpButton>
-      </Show>
-      <Show when="signed-in">
-        <UserButton />
-      </Show>
-      <ModeToggle />
+      <h1>Welcome to Twitter</h1>
     </div>
   );
 }
