a1738f741882534e12e57cab63a727f682127b1d
chamodhlv <chamodh300@gmail.com>
2026-08-10 16:04:07 +0530

    feat: enhance ProfilePage to fetch user posts and liked posts, and check follow status

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -1,4 +1,10 @@
-import { getProfileByUsername } from "@/actions/Profile.action";
+import {
+  getProfileByUsername,
+  getUserLikedPosts,
+  getUserPosts,
+  isFollowing,
+} from "@/actions/Profile.action";
+import { notFound } from "next/navigation";
 
 export async function generateMetadata({
   params,
@@ -15,7 +21,17 @@ export async function generateMetadata({
   };
 }
 
-function ProfilePage({ params }: { params: { username: string } }) {
+async function ProfilePage({ params }: { params: { username: string } }) {
+  const user = await getProfileByUsername(params.username);
+
+  if (!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
+
   return <div>page</div>;
 }
 
