4fc97d50641d532545ee09ce120ab7003ee9cdd0
chamodhlv <chamodh300@gmail.com>
2026-08-10 15:54:01 +0530

    feat: update metadata for RootLayout and implement dynamic metadata generation in ProfilePage

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -1,4 +1,19 @@
-import React from "react";
+import { getProfileByUsername } from "@/actions/Profile.action";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const user = await getProfileByUsername(params.username);
+
+  if (!user) return;
+
+  return {
+    title: `${user.name} (@${user.username})`,
+    description: user.bio || `Check out ${user.name}'s profile on Twitter.`,
+  };
+}
 
 function ProfilePage({ params }: { params: { username: string } }) {
   return <div>page</div>;
